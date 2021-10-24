cb04233e464d1400f3918e2e7c7b1486db152766
Alexandra Shestakova <alexandra_shestakova@epam.com>
2021-10-24 14:03:32 +0300

    feat: added display of comments

--- components/PostComments/index.tsx
+++ components/PostComments/index.tsx
@@ -3,22 +3,12 @@ import { Comment } from '../../components/Comment';
 import { Divider, Paper, Tab, Tabs, Typography } from '@material-ui/core';
 import styles from './PostComments.module.scss';
 import { AddCommentForm } from '../AddCommentForm';
+import data from '../../data';
 
-type CommentObj = {
-  text: string;
-  id: number;
-  createdAt: string;
-  user: {
-    fullName: string;
-    avatar: string;
-  };
-};
+export const PostComments: React.FC = () => {
+  const [activeTab, setActiveTab] = React.useState(0);
+  const comments = data.comments[activeTab === 0 ? 'popular' : 'new'];
 
-interface PostCommentsProps {
-  items: CommentObj[];
-}
-
-export const PostComments: React.FC<PostCommentsProps> = ({ items }) => {
   return (
     <Paper elevation={0} className="mt-40 p-30">
       <div className="container">
@@ -26,8 +16,9 @@ export const PostComments: React.FC<PostCommentsProps> = ({ items }) => {
           42 комментария
         </Typography>
         <Tabs
+          onChange={(_, newValue) => setActiveTab(newValue)}
           className="mt-20"
-          value={0}
+          value={activeTab}
           indicatorColor="primary"
           textColor="primary"
         >
@@ -37,7 +28,7 @@ export const PostComments: React.FC<PostCommentsProps> = ({ items }) => {
         <Divider />
         <AddCommentForm />
         <div className="mb-20" />
-        {items.map((obj) => (
+        {comments.map((obj) => (
           <Comment
             key={obj.id}
             user={obj.user}
--- components/SideComments/index.tsx
+++ components/SideComments/index.tsx
@@ -1,86 +1,24 @@
 import React from 'react';
 import ArrowRightIcon from '@material-ui/icons/NavigateNextOutlined';
-
+import { CommentItem } from './CommentItem';
+import data from '../../data';
 import styles from './SideComments.module.scss';
-
-export const comments = [
-  {
-    id: 1,
-    user: {
-      fullName: 'Вася Пупкин',
-      avatar:
-        'https://leonardo.osnova.io/104b03b4-5173-fd9f-2af9-b458dddc4a23/-/scale_crop/108x108/-/format/webp/',
-    },
-    text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
-    post: {
-      title: 'Какая у вас дома ванна?',
-    },
-    createdAt: new Date().toString(),
-  },
-  {
-    id: 2,
-    user: {
-      fullName: 'Вася Пупкин',
-      avatar:
-        'https://leonardo.osnova.io/f2b74c5b-6387-15f0-e6d7-1d2eacc52c09/-/scale_crop/64x64/-/format/webp/',
-    },
-    text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
-    post: {
-      title: 'Какая у вас дома ванна?',
-    },
-    createdAt: new Date().toString(),
-  },
-  {
-    id: 3,
-    user: {
-      fullName: 'Вася Пупкин',
-      avatar:
-        'https://leonardo.osnova.io/7161cd97-ffdc-1930-2b13-419de941c777/-/scale_crop/64x64/-/format/webp/',
-    },
-    text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
-    post: {
-      title: 'Какая у вас дома ванна?',
-    },
-    createdAt: new Date().toString(),
-  },
-];
-
-interface CommentItemProps {
-  user: {
-    fullName: string;
-  };
-  text: string;
-  post: {
-    title: string;
-  };
-}
-
-const CommentItem: React.FC<CommentItemProps> = ({ user, text, post }) => {
-  return (
-    <div className={styles.commentItem}>
-      <div className={styles.userInfo}>
-        <img src="https://leonardo.osnova.io/598fc957-a3f6-598c-b6f9-a033c3941d12/-/scale_crop/64x64/-/format/webp/" />
-        <a href="#">
-          <b>{user.fullName}</b>
-        </a>
-      </div>
-      <p className={styles.text}>{text}</p>
-      <a href="#">
-        <span className={styles.postTitle}>{post.title}</span>
-      </a>
-    </div>
-  );
-};
+import clsx from 'clsx';
 
 export const SideComments = () => {
+  const [visible, setVisible] = React.useState(true);
+
+  const toggleVisible = () => setVisible(!visible);
+
   return (
-    <div className={styles.root}>
-      <h3>
+    <div className={clsx(styles.root, !visible && styles.rotated)}>
+      <h3 onClick={toggleVisible}>
         Комментарии <ArrowRightIcon />
       </h3>
-      {comments.map((obj) => (
-        <CommentItem key={obj.id} {...obj} />
-      ))}
+      {visible &&
+        data.comments.popular.map((obj) => (
+          <CommentItem key={obj.id} {...obj} />
+        ))}
     </div>
   );
 };
